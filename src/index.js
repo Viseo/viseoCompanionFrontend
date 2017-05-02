a285ea60840e0ea4ee5f46def8f9820e57c04da6
alexandre abrantes <alexandre.abts@gmail.com>
2017-05-02 14:17:43 +0200

    Created calendar scene

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import SignIn from './scenes/signIn';
 import App from './scenes/App';
 import './index.css';
 import { Router, Route, browserHistory, IndexRoute} from 'react-router';
+import Calendar from "./scenes/Calendar";
 
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={SignIn}/>
+            <IndexRoute component={Calendar}/>
+            <Route path="calendar" component={Calendar}/>
             <Route path="signIn" component={SignIn} db={db}/>
             <Route path="add" component={AddEvent} />
         </Route>
